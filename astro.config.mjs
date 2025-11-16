5c07bb8f87bba9a1c8813e9d1ad5e95a10c6484e
Romain Delhommais <romain@delhommais.com>
2025-11-16 16:13:23 +0100

    chore(config): remove unused config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  server: {
-    host: true,
-  },
-
-  vite: {
-    server: {
-      allowedHosts: [
-        "luminary.local", // local dev
-      ],
-    },
-  },
-});
+export default defineConfig({});
