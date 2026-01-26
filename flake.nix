67628e3b35ca57b78e614f2b6d00a64092d41721
Romain Delhommais <romain@delhommais.com>
2026-01-26 15:01:35 +0100

    feat(nix): add devshell with pnpm

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "My Astro website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs_22
+          nodePackages.pnpm
+          typescript
+          astro-language-server
+          nodePackages.typescript-language-server
+        ];
+
+        shellHook = ''
+          echo "Astro Development Environment Active"
+          if [ ! -d "node_modules" ]; then
+            pnpm install
+          fi
+          export PATH="$PWD/node_modules/.bin:$PATH"
+        '';
+      };
+    };
+}
