b074f9ac4a1f1adf52dd72918f80bb5147e5a0bd
Romain Delhommais <romain@delhommais.com>
2026-02-24 00:04:49 +0100

    feat(likes): add likes page with anime and music collections

---
+++ src/data/likes.ts
@@ -0,0 +1,56 @@
+import type { ImageMetadata } from "astro";
+
+import plasticMemories from "../assets/likes/plastic_memories.webp";
+import quintuplets from "../assets/likes/quintuplets.webp";
+import pancreas from "../assets/likes/pancreas.webp";
+import gu1t4r from "../assets/likes/gu1t4r.webp";
+import taylorSwift from "../assets/likes/taylor_swift.webp";
+
+export type LikeItem = {
+  name: string;
+  url: string;
+  image: ImageMetadata;
+};
+
+export type LikeCategory = {
+  title: string;
+  items: LikeItem[];
+};
+
+export const likesData: LikeCategory[] = [
+  {
+    title: "anime / manga",
+    items: [
+      {
+        name: "Plastic Memories",
+        url: "https://anilist.co/anime/20872/Plastic-Memories/",
+        image: plasticMemories,
+      },
+      {
+        name: "The Quintessential Quintuplets",
+        url: "https://anilist.co/manga/100230/The-Quintessential-Quintuplets/",
+        image: quintuplets,
+      },
+      {
+        name: "I Want to Eat Your Pancreas",
+        url: "https://anilist.co/anime/99750/I-Want-to-Eat-Your-Pancreas/",
+        image: pancreas,
+      },
+    ],
+  },
+  {
+    title: "music",
+    items: [
+      {
+        name: "GU1T4R",
+        url: "https://www.last.fm/music/GU1T4R",
+        image: gu1t4r,
+      },
+      {
+        name: "Taylor Swift",
+        url: "https://www.last.fm/music/Taylor+Swift",
+        image: taylorSwift,
+      },
+    ],
+  },
+];
